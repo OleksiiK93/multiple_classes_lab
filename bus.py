1fb6fb620ccafd1b16d08a3ee89d25bda1fa6a77
Oleksii Koliukh <oleksii.koliukh@gmail.com>
2024-01-19 14:42:39 +0000

    Complete part B

--- bus.py
+++ bus.py
@@ -2,5 +2,19 @@ class Bus:
     def __init__(self, route_number, destination) -> None:
         self.route_number = route_number
         self.destination = destination
+        self.passengers = list()
+
     def drive(self):
         return "Brum brum"
+    
+    def passenger_count(self):
+        return len(self.passengers)
+    
+    def pick_up(self, passenger):
+        self.passengers.append(passenger)
+
+    def drop_off(self, passenger):
+        self.passengers.remove(passenger)
+    
+    def empty_bus(self):
+        self.passengers = list()
